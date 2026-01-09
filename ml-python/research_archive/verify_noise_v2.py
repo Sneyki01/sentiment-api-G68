d0f9c09e652d0687db6fea658377e8eaf4763bf6
Alxs68 <oliveros.alexis727@gmail.com>
2026-01-09 15:32:40 -0500

    fix: restaurar README, limpiar .gitignore, eliminar archivos innecesarios y fijar versiones

--- ml-python/research_archive/verify_noise_v2.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = "http://localhost:8080/predict/sentiment"
-data = {"text": "%&$/Cucarachas en la camacc¡?=)(/%"}
-response = requests.post(url, json=data)
-print(response.json())
